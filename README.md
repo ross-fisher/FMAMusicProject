ae800e53ce2be008c8b9cae0e8127c6f97d805f9
Ross Fisher <rcfisher1097@gmail.com>
2019-09-23 18:28:24 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# ds-dash-template
+
+This is an interactive plotly dash webapp which predicts number of listens for a music track based on the FMA (Free Music Archive) dataset, available at https://github.com/mdeff/fma/. 
+```bash
+cd external
+curl -O https://os.unil.cloud.switch.ch/fma/fma_metadata.zip
+unzip fma_metadata
+```
---
+++ pages/test5.py
@@ -0,0 +1,57 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+import pandas as pd
+import plotly.graph_objs as go
+import plotly.express as px
+from app import app
+
+all_options = {
+    'America': ['New York City', 'San Francisco', 'Cincinnati'],
+    'Canada': [u'Montréal', 'Toronto', 'Ottawa']
+}
+
+
+@app.callback(
+    Output('cities-dropdown', 'options'),
+    [Input('countries-dropdown', 'value')])
+def set_cities_options(selected_country):
+    return [{'label': i, 'value': i} for i in all_options[selected_country]]
+
+
+@app.callback(
+    Output('cities-dropdown', 'value'),
+    [Input('cities-dropdown', 'options')])
+def set_cities_value(available_options):
+    return available_options[0]['value']
+
+
+@app.callback(
+    Output('display-selected-values', 'children'),
+    [Input('countries-dropdown', 'value'),
+     Input('cities-dropdown', 'value')])
+def set_display_children(selected_country, selected_city):
+    return u'{} is a city in {}'.format(
+        selected_city, selected_country,
+    )
+
+
+
+layout = html.Div([
+    html.H1('Chained Callbacks'),
+    dcc.RadioItems(
+        id='countries-dropdown',
+        options=[{'label': k, 'value': k} for k in all_options.keys()],
+        value='America'
+    ),
+
+    html.Hr(),
+
+    dcc.RadioItems(id='cities-dropdown'),
+
+    html.Hr(),
+
+    html.Div(id='display-selected-values')
+])
